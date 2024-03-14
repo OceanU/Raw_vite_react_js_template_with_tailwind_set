6c34172f1677be6e88f2dc29f49437362e75aaf6
OceanU <oceanu.ionut@yahoo.ro>
2024-03-14 13:06:39 +0200

    tailwind setting up an other
    Installing and setting up tailwind for this template,
    Add visualization for tailwind measures (width: sm, md, lg)
    Setting up bigger screens sizes breakpoints: 3xl, 4xl

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  //function to see viewport width according to tailwind breakpoints - TO BE DELETED BEFORE BUILD !!!!!!
+  function updateWindowSize() {
+    const windowWidth = window.innerWidth;
+    // const windowHeight = window.innerHeight;
+    console.log(`Window Width: ${windowWidth}px`);
+
+    if ((windowWidth >= 640) && (windowWidth < 768)) {
+        console.log('sm > 640px <768px')
+    } else if ((windowWidth >= 768) && (windowWidth < 1024)) {
+        console.log('md > 768px < 1024px ')
+    } else if ((windowWidth >= 1024) && (windowWidth < 1280)) {
+        console.log('lg - >1024px <1280px ')
+    } else if ((windowWidth >= 1280) && (windowWidth < 1536)) {
+        console.log('xl - >1280px <1536px ')
+    } else if ((windowWidth >= 1536) && (windowWidth < 1920)) {
+        console.log('2xl - >1536px  <1920px')
+    } else if ((windowWidth >= 1920) && (windowWidth < 2560)) {
+        console.log('3xl - >1920px <2560px ')
+    } else if (windowWidth >= 2560) {
+        console.log('4xl - >2560px ')
+    }
+
+}
+
+// Initial call to set the initial window size
+updateWindowSize();
+
+// Event listener for window resize
+window.addEventListener('resize', updateWindowSize);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1 className="text-3xl font-bold underline">
+      Hello world! This template is already set to work with TAILWIND
+    </h1>
     </>
   )
 }
